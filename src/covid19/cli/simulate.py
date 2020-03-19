bf9ce400aeddead939dda03fcb137c4f95fa3e5e
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-19 20:50:52 +0100

    Use tabulate to format tables

--- src/covid19/cli/simulate.py
+++ src/covid19/cli/simulate.py
@@ -1,4 +1,8 @@
+import dataclasses
+from typing import Dict
+
 import click
+from tabulate import tabulate
 
 from .main import main
 from .utils import print_heading
@@ -8,31 +12,21 @@ from ..data import Population, populations
 
 def print_predictions(population: Population) -> None:
     print_heading(population.name)
-    print()
-    print(
-        "  ".join(
-            (
-                "DAY",
-                f"{'DATE':12}",
-                f"{'INFEST':6}%",
-                f"{'CASES':8}",
-                f"{'ACC     ':8}",
-            )
-        )
-    )
 
-    for state in simulation.simulate(population):
-        print(
-            "  ".join(
-                (
-                    f"{state.days:3}",
-                    f"{state.date:%b %d %Y}",
-                    f"{state.infestation:6.2f}%",
-                    f"{state.cases:8}",
-                    f"{state.accumulated_infections:8}",
-                )
-            )
-        )
+    headers = [
+        "days",
+        "date",
+        "accumulated_infections",
+        "infestation",
+        "cases",
+        "recoveries",
+        "probability",
+        "infections",
+    ]
+    states = [dataclasses.asdict(state) for state in simulation.simulate(population)]
+    table = [[state[header] for header in headers] for state in states]
+
+    print(tabulate(table, headers, floatfmt=".2f"))
 
 
 @main.command()
