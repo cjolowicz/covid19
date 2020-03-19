c4b65f9f932a87638dc1d24ca7ebe4533b6e321a
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-19 17:02:44 +0100

    Blacken

--- src/covid19/simulation.py
+++ src/covid19/simulation.py
@@ -31,7 +31,7 @@ class Simulation:
         self._window.append(infections)
         self._cases += infections - recoveries
 
-        #print(f"""FEED {infections} infections => {100 * self._probability:.2f}% probability, {recoveries} recoveries, {self._cases} cases""", end="")
+        # print(f"""FEED {infections} infections => {100 * self._probability:.2f}% probability, {recoveries} recoveries, {self._cases} cases""", end="")
         return self._cases
 
     def _update_probability(self, infections: int) -> None:
@@ -49,7 +49,7 @@ class Simulation:
         self._cases += infections - recoveries
         self._window.append(infections)
 
-        #print(f"""STEP => {100 * infestation:.2f}% infestation, {infections} infections, {recoveries} recoveries, {self._cases} cases""", end="")
+        # print(f"""STEP => {100 * infestation:.2f}% infestation, {infections} infections, {recoveries} recoveries, {self._cases} cases""", end="")
         return self._cases
 
     def run(self) -> Iterator[int]:
