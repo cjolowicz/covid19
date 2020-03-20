68af7a287ed954cd398324681fb1becabfaf47bc
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-20 12:42:26 +0100

    Improve plot

--- src/covid19/cli/plot.py
+++ src/covid19/cli/plot.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
+import datetime
 
 import click
+import matplotlib.pyplot as plt
+import matplotlib.dates
 
 from .main import main
 from .. import data, simulation
@@ -11,15 +13,40 @@ def plot_predictions(population: data.Population, with_immunity: bool) -> None:
         return 100 * value / population.population
 
     states = list(simulation.simulate(population, with_immunity))
-    plt.title(population.name)
-    plt.xlabel("days")
+    last_updated = population.start + datetime.timedelta(days=len(population.cases))
+    dates = [
+        population.start + datetime.timedelta(days=state.days - 1) for state in states
+    ]
+    cases = [percentage(state.cases) for state in states]
+    immune = [percentage(state.immune) for state in states]
+    infections = [percentage(state.infections) for state in states]
+    recoveries = [percentage(state.recoveries) for state in states]
+
+    plt.plot(
+        dates, cases, label="cases", color="tab:orange",
+    )
+    plt.plot(
+        dates, immune, label="immune", color="tab:blue",
+    )
+    plt.plot(
+        dates, infections, label="infections", color="tab:red",
+    )
+    plt.plot(
+        dates, recoveries, label="recoveries", color="tab:green",
+    )
+
+    plt.suptitle(f"COVID-19 simulation for {population.name}", fontsize=14)
+    plt.title(f"Last updated: {last_updated:%b %d, %Y}", fontsize=10)
     plt.ylabel("% of population")
     plt.grid(True)
-    plt.plot([percentage(state.cases) for state in states], label="cases")
-    plt.plot([percentage(state.immune) for state in states], label="immune")
-    plt.plot([percentage(state.infections) for state in states], label="infections")
-    plt.plot([percentage(state.recoveries) for state in states], label="recoveries")
     plt.legend()
+
+    locator = matplotlib.dates.MonthLocator()
+    formatter = matplotlib.dates.DateFormatter("%b")
+    xaxis = plt.gca().xaxis
+    xaxis.set_major_locator(locator)
+    xaxis.set_major_formatter(formatter)
+
     plt.show()
 
 
