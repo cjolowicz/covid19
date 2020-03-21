20f4d3fa5c7c5323329e7fc3e03d0b1e51f6422f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-21 22:15:03 +0100

    Add cases%, difference, ratio to cli.data

--- src/covid19/cli/data.py
+++ src/covid19/cli/data.py
@@ -10,16 +10,31 @@ from .utils import heading
 from .. import populations
 
 
+def format_row(
+    population: populations.Population, days: int, cases: int
+) -> Dict[str, str]:
+    date = population.start + datetime.timedelta(days)
+    previous = population.cases[days - 1] if days > 0 else 0
+    return {
+        "date": f"{date:%b %d %Y}",
+        "cases": cases,
+        "cases%": f"{100 * cases / population.population:.3f}%",
+        "difference": cases - previous,
+        "ratio": cases / previous if previous != 0 else 0,
+    }
+
+
 def print_population(population: populations.Population) -> None:
     table = [
-        {"date": population.start + datetime.timedelta(days), "cases": cases,}
+        format_row(population, days, cases)
         for days, cases in enumerate(population.cases)
     ]
     text = f"""\
 {heading(population.name)}
 
 population = {humanize.intword(population.population)}
-{tabulate(table, headers="keys")}
+
+{tabulate(table, headers="keys", floatfmt=".2f")}
 """
     print(text)
 
